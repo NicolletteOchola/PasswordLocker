92a66c7829eb6b41680e9437862b7d7edd9bebba
Nicole Ocholla <nicoleochola@gmail.com>
2019-12-16 10:54:24 +0300

    Created a test class that tests cases of the user and alsp a function to create a new user account before each test.

--- user_details_test.py
+++ user_details_test.py
@@ -1,18 +1,18 @@
 import unittest
 import pyperclip
-from user_credentials import User, Credential
+from user_details import User, Credential
 
-# class TestUser(unittest.TestCase):
-# 	'''
-# 	Test class that defines test cases for the user class behaviours.
-# 	Args:
-# 	    unittest.TestCase: helps in creating test cases
-# 	'''
-# 	def setUp(self):
-# 		'''
-# 		Function to create a user account before each test
-# 		'''
-# 		self.new_user = User('Mary','Ng\'ang\'a','pswd100')
+class TestUser(unittest.TestCase):
+	'''
+	Test class that defines test cases for the user class behaviours.
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+	def setUp(self):
+		'''
+		Function to create a user account before each test
+		'''
+		self.new_user = User('Nicole','Ochola','pass103')
 
 # 	def test__init__(self):
 # 		'''
